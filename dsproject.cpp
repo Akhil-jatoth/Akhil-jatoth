8868f2f6221835f7303c917e5696056be141889e
Akhil-jatoth <akhijt2420@gmail.com>
2025-05-01 17:52:22 +0530

    Update dsproject.cpp

--- dsproject.cpp
+++ dsproject.cpp
@@ -15,3 +15,32 @@ bool compareIgnoreCase(string a, string b) {
     }
     return true;
 }
+class Taxi 
+{
+    public:
+        int taxiID;
+        string driverName;
+        string vehicleNumber;
+        string dropLocation;
+        int driverID;
+        int ETA; // (Estimated time to arrival)
+        float additionalCharges;
+        float distance; // in km
+
+        Taxi(int id, string driver, int dID, string vehicle, string location, int eta, float charges, float dist) {
+            SetConsoleOutputCP(CP_UTF8);
+            taxiID = id;
+            driverName = driver;
+            driverID = dID;
+            vehicleNumber = vehicle;
+            dropLocation = location;
+            ETA = eta;
+            additionalCharges = charges;
+            distance = dist;
+            cout << " Taxi " << taxiID << " (" << driverName << ") is ready at the stand!" << endl;
+        }
+    
+        ~Taxi() {
+            SetConsoleOutputCP(CP_UTF8);
+            cout << " Taxi " << taxiID << " (" << driverName << ") has completed the service!" << endl;
+        }
